6a53ee1cc0fb637daf3b25ea8cab9bdada7a8ac1
Arbaoui Mehdi <arbaoui.mehdi@gmail.com>
2019-06-18 12:50:11 +0000

    Refactor creating posts automatically

--- gatsby/gatsby-wordpress/gatsby-node.js
+++ gatsby/gatsby-wordpress/gatsby-node.js
@@ -1,65 +1,56 @@
-const _ = require('lodash');
-const Promise = require('bluebird');
-const path = require('path');
-const slash = require('slash');
+const path = require(`path`)
+const slash = require(`slash`)
 
 // Implement the Gatsby API “createPages”. This is
 // called after the Gatsby bootstrap is finished so you have
 // access to any information necessary to programmatically
 // create pages.
-// Will create pages for Wordpress pages (route : /{slug})
-// Will create pages for Wordpress posts (route : /post/{slug})
-exports.createPages = ({
-  graphql,
-  actions,
-}) => {
-  const {
-    createPage,
-  } = actions;
-  return new Promise((resolve, reject) => {
-    // The “graphql” function allows us to run arbitrary
-    // queries against the local Wordpress graphql schema. Think of
-    // it like the site has a built-in database constructed
-    // from the fetched data that you can run queries against.
+// Will create pages for WordPress pages (route : /{slug})
+// Will create pages for WordPress posts (route : /post/{slug})
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
 
-    // ==== POSTS (WORDPRESS NATIVE AND ACF) ====
-    graphql(`
+  // The “graphql” function allows us to run arbitrary
+  // queries against the local Gatsby GraphQL schema. Think of
+  // it like the site has a built-in database constructed
+  // from the fetched data that you can run queries against.
+  const result = await graphql(`
     {
       allWordpressPost {
         edges {
           node {
             id
-            slug
+            path
             status
             template
             format
-            content
           }
         }
       }
     }
-    `,
-    ).then((result) => {
-      if (result.errors) {
-        console.log(result.errors);
-        reject(result.errors);
-      }
-      const postTemplate = path.resolve('./src/templates/post.js');
-      // We want to create a detailed page for each
-      // post node. We'll just use the Wordpress Slug for the slug.
-      // The Post ID is prefixed with 'POST_'
-      _.each(result.data.allWordpressPost.edges, (edge) => {
-        createPage({
-          path: `/blog/${edge.node.slug}`,
-          component: slash(postTemplate),
-          context: {
-            id: edge.node.id,
-          },
-        });
-      });
-      resolve();
-    });
-    // ==== END POSTS ====
-  });
+  `)
 
-};
+  // Check for any errors
+  if (result.errors) {
+    throw new Error(result.errors)
+  }
+
+  // Access query results via object destructuring
+  const { allWordpressPage, allWordpressPost } = result.data
+
+  // Create Posts
+  const postTemplate = path.resolve(`./src/templates/post.js`)
+  // We want to create a detailed page for each post node.
+  // The path field stems from the original WordPress link
+  // and we use it for the slug to preserve url structure.
+  // The Post ID is prefixed with 'POST_'
+  allWordpressPost.edges.forEach(edge => {
+    createPage({
+      path: edge.node.path,
+      component: slash(postTemplate),
+      context: {
+        id: edge.node.id,
+      },
+    })
+  })
+}
